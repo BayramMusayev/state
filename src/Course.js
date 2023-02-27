659e254f486ef38209ca74fa67f41e936b4d351f
BayramMusayev <bayram01992@gmail.com>
2023-02-27 20:29:56 +0400

    state

---
+++ src/Course.js
@@ -0,0 +1,18 @@
+import Angular from './images/angular.jpg';
+import Bootstrap from './images/bootstrap5.png';
+import Ccsharp from './images/ccsharp.png';
+import KompleWeb from './images/kompleweb.jpg';
+import './Course.css';
+
+
+const courseMap = {
+    Angular, Bootstrap, Ccsharp, KompleWeb
+}
+
+function Course({courseName}) {
+  return <div className='courseDiv'>
+    <img className='courseImg' src={courseMap[courseName]} alt="course" />
+  </div>;
+}
+
+export default Course;
